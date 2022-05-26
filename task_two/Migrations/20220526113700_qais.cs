c97f492628f850bf328e55056f665b21c214f15e
ibrahem-abualfoul <76413554+ibrahem-abualfoul@users.noreply.github.com>
2022-05-26 14:38:52 +0300

    final edit

---
+++ task_two/Migrations/20220526113700_qais.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace task_two.Migrations
+{
+    public partial class qais : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
